9073f1a93b506bf8cfae1689c478a96682f97bbf
Louzh007 <lou54321@icloud.com>
2026-04-14 17:46:53 +0800

    Second update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-import { name } from "./package.json";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${name}/`,
+  base: "/Parking-lot-game/",
 });
